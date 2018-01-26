5560ad208dda9a307c340e0a3f708092243e8c2f
asaduddin <asaduddin.z@samsung.com>
2018-01-26 18:48:18 +0700

    initial commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const numSquares = 6;
+let colors = [];
+const colorDisplay = document.getElementById('colorDisplay');
+
+let generateRandomColors = (num) => {
+    let arr = [];
+    num.forEach(element => {
+        arr.push(element)
+    });
+}
+
+let randomColor = () => {
+    
+}
